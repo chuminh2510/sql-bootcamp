239671bb62c5c37abcab5579c803edb3525592d4
Chu Van Minh <chuminhuet@gmail.com>
2022-08-03 16:53:11 +0700

    Treenode sql

--- leetcode/tree-node.sql
+++ leetcode/tree-node.sql
@@ -1,1 +1,10 @@
 -- https://leetcode.com/problems/tree-node//
+select
+    id,
+    (case
+        when p_id is null then 'Root'
+        when id in (select distinct p_id from Tree) then 'Inner'
+        else 'Leaf'
+     end
+    )as type
+from Tree
